d93e8ddc402aabc490df736db8ab194ea27352fd
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-04 21:22:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,5 +19,17 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  
+  money_hash = {}
+row_index = 0
+while row_index < directors_database.length do
+column_index = 0
+total_gross = 0
+while column_index < directors_database[row_index][:movies].length do
+total_gross += directors_database[row_index][:movies][column_index][:worldwide_gross]
+column_index += 1
+end
+money_hash[directors_database[row_index][:name]] = total_gross
+row_index +=1 
+end
+money_hash
 end
